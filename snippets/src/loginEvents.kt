6eed854d40fdf9603ae34e0bc52535358a52598e
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-08-10 09:59:38 +0200

    Update loginEvents.kt

--- snippets/src/loginEvents.kt
+++ snippets/src/loginEvents.kt
@@ -9,27 +9,41 @@ object LogoutEvent: Event
 
 data class LoginEvent(val userName: String, val token: String): Event
 
+/**
+ * Use 'collectEvents' to subscribe to all events of type [LogoutEvent]
+ */
 suspend fun listenForLogout() = collectEvents<LogoutEvent> {
     println("User logged out")
 }
 
+/**
+ * Use 'collectEvents' to subscribe to all events of type [LoginEvent]
+ */
 suspend fun listenForLogin() = collectEvents<LoginEvent> { event ->
     println("User: ${event.userName} logged in")
 }
 
+/**
+ * Example function 'login' which will pretend to login a user and then
+ * emits a [LoginEvent]
+ */
 suspend fun login(userName: String, password: String) {
     val token = httpClient().login(userName, password) ?: return
     LoginEvent(userName, token).emit()
                     //          ^
-                    // Actually emit the event and suspend until
+                    // emit the event and suspend until
                     // All listeners have finished processing this event
 }
 
+/**
+ * Example function 'logout' which will pretend to delete user data and then
+ * emits a [LogoutEvent]
+ */
 suspend fun logout() {
     deleteUserData()
     LogoutEvent.emit()
     //          ^
-    // Actually emit the event and suspend until
+    // emit the event and suspend until
     // All listeners have finished processing this event
 }
 //End
