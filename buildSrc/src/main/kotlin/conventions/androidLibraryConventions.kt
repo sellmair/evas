87e5be604b3a45cdfb9ca67514ebd743720db427
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-12-20 14:17:57 +0100

    Target JVM 11, using a 21 compiler

--- buildSrc/src/main/kotlin/conventions/androidLibraryConventions.kt
+++ buildSrc/src/main/kotlin/conventions/androidLibraryConventions.kt
@@ -1,6 +1,7 @@
 package conventions
 
 import com.android.build.gradle.LibraryExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
@@ -12,6 +13,11 @@ fun Project.androidLibraryConventions() {
             defaultConfig {
                 minSdk = 15
                 namespace = "io.sellmair.${project.name}"
+            }
+
+            compileOptions {
+                this.sourceCompatibility = JavaVersion.VERSION_11
+                this.targetCompatibility = JavaVersion.VERSION_11
             }
         }
 
--- buildSrc/src/main/kotlin/conventions/kotlinMultiplatformConventions.kt
+++ buildSrc/src/main/kotlin/conventions/kotlinMultiplatformConventions.kt
@@ -1,25 +1,26 @@
 package conventions
 
 import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.AbstractTestTask
 import org.gradle.api.tasks.testing.Test
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 internal fun Project.kotlinMultiplatformConventions() {
     plugins.withType<KotlinMultiplatformPluginWrapper>().all {
         extensions.configure<KotlinMultiplatformExtension> {
-            /* Targets */
-
 
             /*
             Jvm options
              */
-            jvmToolchain(17)
+            jvmToolchain(21)
 
             /*
             Kotlin Compiler Options
@@ -29,6 +30,18 @@ internal fun Project.kotlinMultiplatformConventions() {
                 if (!project.path.startsWith(":samples") && !project.path.startsWith(":snippets")) {
                     explicitApi()
                 }
+            }
+
+            tasks.withType<KotlinJvmCompile>().configureEach {
+                compilerOptions {
+                    this.jvmTarget.set(JvmTarget.JVM_11)
+                    this.freeCompilerArgs.add("-Xjdk-release=11")
+                }
+            }
+
+            tasks.withType<JavaCompile>().configureEach {
+                sourceCompatibility = "11"
+                targetCompatibility = "11"
             }
         }
 
